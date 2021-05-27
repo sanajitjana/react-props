52f2d6a11c4658f97bde793d8ea26c51056a693c
Sanajt Jana <sanajitjana1402@gmail.com>
2021-05-27 19:39:45 +0530

    Data pass from array of an object

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import Cards from "./Card";
 import Navbar from "./Navbar";
+import Blog_Data from "./Blogdata";
 
+console.log(Blog_Data[0]);
 function App() {
   return (
     <>
@@ -11,20 +13,35 @@ function App() {
         <Row>
           <Col md={3}>
             <Cards
-              imgsrc="https://picsum.photos/200/150"
-              title="Blog Title"
-              description="Some quick example text to build on the card title and make up the bulk of the card's content."
-              link="https://picsum.photos/200/150"
+              imgsrc={Blog_Data[0].data_imgsrc}
+              title={Blog_Data[0].data_title}
+              description={Blog_Data[0].data_description}
+              link={Blog_Data[0].data_link}
             />
           </Col>
           <Col md={3}>
-            <Cards />
+            <Cards
+              imgsrc={Blog_Data[1].data_imgsrc}
+              title={Blog_Data[1].data_title}
+              description={Blog_Data[1].data_description}
+              link={Blog_Data[1].data_link}
+            />
           </Col>
           <Col md={3}>
-            <Cards />
+            <Cards
+              imgsrc={Blog_Data[2].data_imgsrc}
+              title={Blog_Data[2].data_title}
+              description={Blog_Data[2].data_description}
+              link={Blog_Data[2].data_link}
+            />
           </Col>
           <Col md={3}>
-            <Cards />
+            <Cards
+              imgsrc={Blog_Data[3].data_imgsrc}
+              title={Blog_Data[3].data_title}
+              description={Blog_Data[3].data_description}
+              link={Blog_Data[3].data_link}
+            />
           </Col>
         </Row>
       </Container>
---
+++ src/Blogdata.js
@@ -0,0 +1,32 @@
+const Blog_Data = [
+  {
+    data_imgsrc: "https://picsum.photos/200/150",
+    data_title: "Blog Title",
+    data_description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    data_link: "https://picsum.photos/200/150",
+  },
+  {
+    data_imgsrc: "https://picsum.photos/200/150",
+    data_title: "Blog Title",
+    data_description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    data_link: "https://picsum.photos/200/150",
+  },
+  {
+    data_imgsrc: "https://picsum.photos/200/150",
+    data_title: "Blog Title",
+    data_description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    data_link: "https://picsum.photos/200/150",
+  },
+  {
+    data_imgsrc: "https://picsum.photos/200/150",
+    data_title: "Blog Title",
+    data_description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    data_link: "https://picsum.photos/200/150",
+  },
+];
+
+export default Blog_Data;
