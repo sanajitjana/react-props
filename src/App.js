2416824c882d3f1631073d64bcc17b23ea41833c
Sanajt Jana <sanajitjana1402@gmail.com>
2021-05-28 17:10:30 +0530

    creating props

--- src/App.js
+++ src/App.js
@@ -5,44 +5,52 @@ import { Row, Col, Container } from "react-bootstrap";
 import Cards from "./Component/Card";
 import Navbar from "./Component/Navbar";
 
-//array
-import Blog_Data from "./Component/Api";
-
-//------ fat arrow fucntion -------
-
-// const nblog = (val) => {
-//   return (
-//     <Col md={4}>
-//       <Cards
-//         imgsrc={val.data_imgsrc}
-//         title={val.data_title}
-//         description={val.data_description}
-//         link={val.data_link}
-//       />
-//     </Col>
-//   );
-// };
-
-console.log(Blog_Data[0]);
 function App() {
   return (
     <>
       <Container>
         <Navbar />
         <Row>
-          {Blog_Data.map((val) => {
-            return (
-              <Col md={4}>
-                <Cards
-                  imgsrc={val.data_imgsrc}
-                  title={val.data_title}
-                  description={val.data_description}
-                  link={val.data_link}
-                />
-              </Col>
-            );
-          })}
-          {/* {Blog_Data.map(nblog)} */}
+          <Col md={4}>
+            <Cards
+              imgsrc="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              title="Travel"
+              description="1. Some quick example text to build on the card title and make up the bulk of the card's content."
+              link="https://unsplash.com/photos/O453M2Liufs"
+            />
+          </Col>
+          <Col md={4}>
+            <Cards
+              imgsrc="https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              title="Restaurant"
+              description="2. Some quick example text to build on the card title and make up the bulk of the card's content."
+              link="https://unsplash.com/photos/M7GddPqJowg"
+            />
+          </Col>
+          <Col md={4}>
+            <Cards
+              imgsrc="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              title="Food"
+              description="3. Some quick example text to build on the card title and make up the bulk of the card's content."
+              link="https://unsplash.com/photos/IGfIGP5ONV0"
+            />
+          </Col>
+          <Col md={4}>
+            <Cards
+              imgsrc="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              title="Beach"
+              description="4. Some quick example text to build on the card title and make up the bulk of the card's content."
+              link="https://unsplash.com/photos/RN6ts8IZ4_0"
+            />
+          </Col>
+          <Col md={4}>
+            <Cards
+              imgsrc="https://images.unsplash.com/photo-1573878734959-7c38a12942ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              title="Office"
+              description="5. Some quick example text to build on the card title and make up the bulk of the card's content."
+              link="https://unsplash.com/photos/6vs4x5g84cs"
+            />
+          </Col>
         </Row>
       </Container>
     </>
@@ -50,11 +58,3 @@ function App() {
 }
 
 export default App;
-
-//below default function-->
-// function myname(a, b) {
-//   a + b;
-// }
-
-//below fat arrow function-->
-// const myname = (a, b) => a + b;
